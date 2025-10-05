5ca850a0cfabcdb5c268cf762a4da74a85c4b4f8
unsureBoolean <73371783+unsureboolean@users.noreply.github.com>
2025-10-05 12:06:14 -0500

    Update OrientationLock.tsx
    Signed-off-by: unsureBoolean <73371783+unsureboolean@users.noreply.github.com>

--- components/OrientationLock.tsx
+++ components/OrientationLock.tsx
@@ -1,7 +1,1 @@
-import React from 'react';
 
-// This component previously locked mobile devices to landscape orientation.
-// It has been disabled to allow users to view the site in any orientation.
-export const OrientationLock: React.FC = () => {
-    return null;
-};
