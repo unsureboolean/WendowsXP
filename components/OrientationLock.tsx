22398b223339bba4d929ca6de89e525540386cdb
unsureBoolean <73371783+unsureboolean@users.noreply.github.com>
2025-10-05 12:20:47 -0500

    Add files via upload
    Signed-off-by: unsureBoolean <73371783+unsureboolean@users.noreply.github.com>

---
+++ components/OrientationLock.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// This component previously locked mobile devices to landscape orientation.
+// It has been disabled to allow users to view the site in any orientation.
+export const OrientationLock: React.FC = () => {
+    return null;
+};
